7db5c0f4f3644fc5f3ceec11a58647e1ef649129
Kanstantsin Krupenka <kanstantsinks@yandex.ru>
2021-02-18 23:25:59 +0300

    Прописанны методы и функции color, colorLine, loadMoney, saveMoney, getInput, getIntInput, win и fail. Подключены библиотеки os и ctypes. Заданы переменные для цвета, money, defaultMoneym и valuta

--- casino678/casino678.py
+++ casino678/casino678.py
@@ -1,3 +1,14 @@
+import os
+import ctypes as ctp
+
+
+valuta = "Rub"
+money = 0
+defaultMoney = 10000
+ctp.windll.Kernel32.GetStdHandle.restype = ctp.c_ulong
+h = ctp.windll.Kernel32.GetStdHandle(ctp.c_ulong(0xFFFFFFF5))
+
+
 def main():
     """
     Главный метод игры.
@@ -9,40 +20,82 @@ def getInput(digit, message):
     """
     digit - возможные для ввода значения в формате строки 0123.
     message - строка-приглашения при входе.
+    ret - введенный пользователем симол.
     Функция ввода значения.
     """
-    pass
+    color(7)
+    ret = ""
+    while (ret == "" or not ret in digit):
+    	ret = input(message)
+    return ret
+
+#print(f"Вы ввели число {getInput('12', 'Введите 1 или 2! ')}")
+
 
 def getIntInput(minimum, maximum, message):
     """
     minimum - нижняя граница диапазона(включительно).
     maximum - верхняя граница диапазона(включительно).
     message - текст строки-приглашения ввода.
+    st - переменная, хранящая введенную строку.
     Функция ввода целого числа.
     Данная функция вызывается в местах, где требуется ввод пользователя, выбор пункта меню или ввод строки.
     """
+    color(7)
+    ret = -1
+    while (ret < minimum or ret > maximum):
+    	st = input(message)
+    	if (st.isdigit()):
+    		ret = int(st)
+    	else:
+    		print("    Введи целое число! ")
+    return ret
 
-    pass
+#a = getIntInput(0, 10, "Введи число от 0 до 10: ")
+#print(a)
+
 
 def loadMoney():
     """
+    moneyInFile - сумма денег игрока полученная из файла
+    defaultMoney - количество денег по умолчанию, при невозможности открыть файл
+    valuta - запись названия игровой валюты.
     Метод загружает сумму средств игрока из файла money.dat.
     """
-    pass
+    try:
+    	file = open("money.dat", "r")
+    	moneyInFile = int(file.mreadLine())
+    	file.close()
+    except FileNoTFoundError:
+    	print(f"Файла сохранения не существует, задано значение {defaultMoney} {valuta}")
+    	moneyInFile = defaultMoney
+    return moneyInFile
+
 
 def saveMoney(moneyToSave):
     """
     moneyToSave - количество денег на счету пользователя.
     Метод записывает сумму средств игрока из аргумента moneyToSave в файл money.dat.
     """
-    pass
+    try:
+    	file = open("money.dat", "w")
+    	file.write(str(moneyToSave))
+    	file.close()
+    except:
+    	print("Ошибка создания файла, наше казино закрывается!")
+    	quit(0)
 
 def color(c):
     """
     с - номер цвета для установки.
     Метод установки цвета текста в консоли.
     """
-    pass
+    ctp.windll.Kernel32.SetConsoleTextAttribute(h, c)
+
+#for i in range(16):
+#	color(i)
+#	print(f"Цвет № {i}")
+
 
 def colorLine(c, s):
     """
@@ -50,13 +103,21 @@ def colorLine(c, s):
     s - строка которая быудет выводится между линиями со звездочками.
     Метод вывода теста приветствия.
     """
-    pass
+    os.system("cls")
+    color(c)
+    print("*" * (len(s) + 2))
+    print(" " + s)
+    print("*" * (len(s) + 2))
 
-def roulette();
+#colorLine(15, "Привет, я такой типа привлекаю внимание.")
+
+
+def roulette():
     """
     Метод вывода меню Рулетки.
     """
     pass
+
 
 def getRoulette(visible):
     """
@@ -65,11 +126,13 @@ def getRoulette(visible):
     """
     pass
 
+
 def dice():
     """
     Метод вывода меню Костей.
     """
     pass
+
 
 def getDice():
     """
@@ -77,11 +140,13 @@ def getDice():
     """
     pass
 
+
 def oneHandBandit():
     """
     Метод вывода меню Однорукого бандита.
     """
     pass
+
 
 def getOneHandBandit(stavka):
     """
@@ -89,6 +154,7 @@ def getOneHandBandit(stavka):
     Функция отображения анимации однорукого бандита, символическое отображение слотов игрового автомата.
     """
     pass
+
 
 def getMaxCount(digit, v1, v2, v3, v4, v5):
     """
@@ -98,25 +164,36 @@ def getMaxCount(digit, v1, v2, v3, v4, v5):
     """
     pass
 
+
 def getOneHandBanditRes():
     """
     Функция, которая расчитывает выигрыш или проигрыш пользователя.
     """
     pass
 
+
 def win(result):
     """
     result - сумма выигранных средств.
+    valuta - запись названия игровой валюты.
+    money - показывает текущее количество денег у игрока.
     Метод выводящий текст при выигрыше.
     """
-    pass
+    color(14)
+    print(f"    Победа за тобой! Выигрыш составляет: {result} {valuta}")
+    print(f"    У тебя на счету: {money} {valuta}")
+
 
 def fail(result):
     """
     result - сумма проигранных средств.
+    valuta - запись названия игровой валюты.
+    money - показывает текущее количество денег у игрока.
     Метод выводящий текст при проигрыше.
     """
-    pass
+    color(12)
+    print(f"    К сожалению, проигрыш! {result} {valuta}")
+    print(f"    У тебя на счету: {money} {valuta}")
+    print("    Обязательно нужно отыграться!")
 
-
-
+input()
